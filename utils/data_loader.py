66d19fbf6e9fe2d869a2bf991072431cb12963bd
Sam <sam9elliss@gmail.com>
2025-04-09 20:16:27 -0600

    figured out how to read all station data at same time, implementing next

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,9 +1,11 @@
 # NOAA CSV loading + validation
 import pandas as pd
 import streamlit as st
+import os
 
 TORNADO_CSV_URL = "./data/1950-2023_actual_tornadoes.csv"
 WEATHER_STATION_TEST_URL = "./data/weather_stations/USC00137844-iowa-spencer.csv"
+WEATHER_STATION_DATA_URL = "./data/weather_stations/"
 
 @st.cache_data
 def load_tornado_data():
@@ -18,13 +20,16 @@ def load_tornado_data():
 
 @st.cache_data
 def load_station_data():
-    df = pd.read_csv(WEATHER_STATION_TEST_URL)
+    stations = list()
+    for filename in os.listdir(WEATHER_STATION_DATA_URL):
+        stations.append(pd.read_csv(WEATHER_STATION_DATA_URL + filename))
+    print(stations)
     print("loaded")
     # dates are stored per month. ie one row per month.
     # ignoring the positional verifications for now. what could go wrong!
-    print(df) # remove later
+    print(stations[0]) # remove later
     print("  ")
-    print(df["LATITUDE"][1]) # this is how you access a single entry 
-    print(df.iloc[0]) # this is how you'd access a single row
-    print(type(df["LATITUDE"]))
-    return df # pretty sure i can ignore whatever the other return statement is doing ?
+    print(stations[0]["LATITUDE"][1]) # this is how you access a single entry 
+    print(stations[0].iloc[0]) # this is how you'd access a single row
+    print(type(stations[0]["LATITUDE"]))
+    return stations # pretty sure i can ignore whatever the other return statement is doing ?
